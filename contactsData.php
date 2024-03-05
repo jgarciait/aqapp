dd072db3dcdab98b5c63e3cebcde7066584aa049
Joel J Garcia Rodriguez <jgarcia.it@outlook.com>
2024-03-05 14:45:18 -0400

    AQMessenger Update

--- contactsData.php
+++ contactsData.php
@@ -1,4 +1,5 @@
 <?php
+$output = ''; // Define $output before appending content
 while ($row = $result->fetch_assoc()) {
     $sql2 = "SELECT * FROM aq_messages 
     WHERE (incoming_msg_id = {$row['user_id']} OR outgoing_msg_id = {$row['user_id']}) 
@@ -16,23 +17,26 @@ while ($row = $result->fetch_assoc()) {
 
     $messageContent = str_replace("\n", "<br>", $messageContent);
     $msg = (strlen($messageContent) > 20) ? substr($messageContent, 0, 20) . '...' : $messageContent;
-    ($outgoing_id == $row2['outgoing_msg_id']) ? $you = "You: " : $msgWho = "";
-    ($row['user_id'] == $row2['incoming_msg_id']) ? $you = "From: " : $msgWho = "";
-        $output .= '<a href="aqMessengerChatArea.php?id=' . $row['user_id'] . '">
-                        <div class="d-flex align-items-center justify-content-between p-3" style="width: 100%;">
-                            <div class="d-flex align-items-center">
-                                <div class="pe-3">
-                                    <img src="core/assets/uploads/profile_images/' . htmlspecialchars($row['profile_image']) . '" alt="Profile Image" style="margin-right: 1rem; width:40px; height:40px; border-radius: 50%; object-fit: cover;">
-                                </div>
-                                <div>
-                                    ' . htmlspecialchars($row['first_name']) . " " . htmlspecialchars($row['last_name']) . " - " . $row['status'] . '
-                                    <div class="status-message">' . $msgWho . $msg . '</div>
-                                </div>
-                            </div>
-                            <span style="margin-bottom: 0; padding: 0;"><i class="fas fa-circle"></i></span>
-                        </div>
-                        <hr>
-                    </a>';
+    
+    if ($outgoing_id == $row2['outgoing_msg_id']) {
+        $msgWho = "You: " . $msg;
+    } else {
+        $msgWho = "From: " . $msg;
     }
-
+    $output .= '<a href="aqMessengerChatArea.php?id=' . $row['user_id'] . '">
+                    <div class="d-flex align-items-center justify-content-between p-3" style="width: 100%;">
+                        <div class="d-flex align-items-center">
+                            <div class="pe-3">
+                                <img src="core/assets/uploads/profile_images/' . htmlspecialchars($row['profile_image']) . '" alt="Profile Image" style="margin-right: 1rem; width:40px; height:40px; border-radius: 50%; object-fit: cover;">
+                            </div>
+                            <div>
+                                ' . htmlspecialchars($row['first_name']) . " " . htmlspecialchars($row['last_name']) . " - " . $row['status'] . '
+                                <div class="status-message">' . htmlspecialchars($msgWho) . '</div>
+                            </div>
+                        </div>
+                        <span style="margin-bottom: 0; padding: 0;"><i class="fas fa-circle"></i></span>
+                    </div>
+                    <hr>
+                </a>';
+}
 ?>
